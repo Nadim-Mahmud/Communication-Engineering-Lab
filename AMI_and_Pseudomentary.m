49bb8f1d0b718a666e61e0063305548346d2f1a4
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-09-24 21:39:44 +0600

    ami pseudomentary theke diff

---
+++ AMI_and_Pseudomentary.m
@@ -0,0 +1,59 @@
+clear;
+
+bits = [1,0,0,1,1,1,0,1,0,1,1,1];
+
+bit_rate=1;
+voltage=5;
+tmp=voltage;
+sign = 1;
+mrk = 1;
+voltage = sign*voltage;
+
+for i = 1:length(bits)
+    if(bits(i)==0)
+        y_level(i) = 0;
+    else 
+        y_level(i) = -voltage;
+        voltage=y_level(i);
+    end
+end
+
+voltage=tmp;
+Time=length(bits)/bit_rate; 
+frequency = 1000;
+period = 1/(frequency*bit_rate);
+time = 0:period:Time;
+x = 1;
+
+for i = 1:length(time)
+    y_value(i)= y_level(x);
+    if time(i)*bit_rate>=x
+        x= x+1;
+    end
+end
+     
+
+plot(time,y_value);
+axis([0 Time -voltage-2 voltage+2]);
+
+
+% demodulation
+
+in=1;
+for j=1:length(time)
+  tmp = y_value(j)/voltage;
+  if time(j)*bit_rate>=in
+      if tmp==0
+        ans_bits(in)=0;
+      else 
+        ans_bits(in)=1;
+      end 
+      in=in+1;
+  end
+ end
+
+
+disp('Demodulation : ')
+disp(ans_bits)
+
+
