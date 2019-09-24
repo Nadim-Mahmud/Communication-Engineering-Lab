ca9164e95606ea4b5851df452b5fc0aa2c247573
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-09-24 20:33:53 +0600

    Bipoler BRZ_I completed

---
+++ Bipoler NRZ-I.m
@@ -0,0 +1,63 @@
+clear;
+
+bits = [1,0,0,1,1,1,0,1,0,1,1,0,1];
+
+bit_rate=1;
+voltage=5;
+sign = 1;
+
+tmp=voltage;
+voltage = sign*voltage;
+
+for i = 1:length(bits)
+    if(bits(i)==0)
+        amplitude(i) = voltage;        
+    else 
+        amplitude(i) = -voltage;
+        voltage=amplitude(i);
+    end
+end
+
+voltage=tmp;
+
+Time=length(bits)/bit_rate; 
+sampling_frequency = 1000;
+sampling_period = 1/sampling_frequency;
+time = 0:sampling_period:Time;
+
+x = 1;
+
+for i = 1:length(time)
+    y_value(i)= amplitude(x);
+    if time(i)*bit_rate>=x
+        x= x+1;
+    end
+end
+     
+plot(time,y_value);
+axis([0 Time -voltage-2 voltage+2]);
+
+
+% demodulation
+
+i=1;
+in=1;
+tmp=1*sign;
+
+for j=1:length(time)
+  dm = y_value(j)/voltage;
+  if time(j)*bit_rate>=in
+      if dm==tmp
+        ans_bits(in)=0;
+      else 
+        ans_bits(in)=1;
+      end
+      tmp=dm;
+      in= in+1;
+  end
+end
+
+disp('Demodulation : ')
+disp(ans_bits)
+
+
