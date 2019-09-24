d030a11ce80216be952e41e18048dd45cd3a89cd
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-09-24 22:42:39 +0600

    HDB3 scrambling completed

--- B8ZS_new.m
+++ B8ZS_new.m
@@ -4,30 +4,26 @@ bits = [0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1,0];
 
 bit_rate=1;
 voltage=5;
-tmp = voltage;
-sign = -1;
-mrk = 1;
+sign = 1;
+
 voltage = sign*voltage;
-cn0=0;
-cn1=0;
-in = 0;
+tmp = voltage;
+cn=0;
 
 for i = 1:length(bits)
     
     if bits(i) == 0
-        cn0 = cn0+1;
+        cn = cn+1;
     else
-        cn1=cn1+1;
-        cn0=0;
+        cn=0;
     end
     
-    if cn0>7
+    if cn>7
         y_level(i-4) = voltage;
         y_level(i-3) = -voltage;
         y_level(i-1) = -voltage;
         y_level(i) = voltage;
-        cn0=0;
-        c1=0;
+        cn=0;
     elseif bits(i)==0
         y_level(i) = 0;
     else 
@@ -38,8 +34,9 @@ end
 
 voltage=tmp;
 Time=length(bits)/bit_rate; 
-frequency = 1000;
-time = 0:.01:Time;
+sampling_frequency = 1000;
+sampling_period = 1/sampling_frequency;
+time = 0:sampling_period:Time;
 x = 1;
 
 for i = 1:length(time)
@@ -55,36 +52,29 @@ axis([0 Time -voltage-2 voltage+2]);
 
 % demodulation
 
-
-
-% demodulation
-
-i=1;
 in=1;
 tmp=sign;
-mr = 1;
 in1 = -1;
 
 for j=1:length(time)
-  dm(i) = y_value(j)/voltage;
-  if time(j)*bit_rate>=i 
-      if dm(i)==0
+  dm = y_value(j)/voltage;
+  if time(j)*bit_rate>=in 
+      if dm==0
         ans_bits(in)=0;
       else
         if in~=in1+1
             ans_bits(in)=1;
         end
-        if tmp== dm(i)&&i>8;
+        if tmp== dm&&i>8;
             in1 = in;
             ans_bits(in) = 0;
             ans_bits(in+1) = 0;
         end
       end 
       
-      if dm(i)~=0
-        tmp = dm(i);
+      if dm~=0
+        tmp = dm;
       end
-      i=i+1;
       in= in+1;
   end
 end
